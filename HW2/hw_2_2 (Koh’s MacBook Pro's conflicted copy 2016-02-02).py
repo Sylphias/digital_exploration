009fe00406b105044dba6f2547a730a4c3f381e1
Sylphias <sylpharyon@gmail.com>
2016-02-02 01:19:17 +0800

    homework 2

---
+++ HW2/hw_2_2 (Koh’s MacBook Pro's conflicted copy 2016-02-02).py
@@ -0,0 +1,8 @@
+# def cToF(C):
+#   return C*1.8+32.0
+
+from math import pi
+def volumeOfCylinder(radius, length):
+  area = float(radius**2*math.pi)
+  return area, area*length
+
--- HW2/hw_2_2.py
+++ HW2/hw_2_2.py
@@ -1,2 +1,20 @@
+#Q1
 def cToF(C):
   return C*1.8+32.0
+#Q2
+from math import pi
+def areaCylinder(radius, length):
+  area = float(radius**2*pi)
+  return area, area*length
+#Q3
+def windChilltemp(ta, v):
+  if ta >= (-58.0) and ta <= 41 and v >= 2:
+    return 35.74 + 0.6215*ta - 35.75*v**0.16 + 0.4275*ta*v**0.16
+#Q4
+def bmi(weight,height):
+  return (weight*0.45359237)/((height*0.0254)**2)
+#Q5
+def investmentVal(amount, annualRate, years):
+  return round(amount*(1+(annualRate/(12*100)))**(years*12), 2)
+
+print investmentVal(1000,4.25,1)
